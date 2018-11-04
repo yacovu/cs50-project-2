1c505533dcadf47b637acbfbc5b5d8d29eba6a9d
yacovu <yacov.uziel@gmail.com>
2018-11-04 11:10:24 +0200

    modify styles in MovieDetailsScreen

--- screens/MovieDetailsScreen.js
+++ screens/MovieDetailsScreen.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, Image } from 'react-native';
 
 export default class MovieDetailsScreen extends React.Component {
    static navigationOptions = ({navigation}) => ({
@@ -18,10 +18,12 @@ export default class MovieDetailsScreen extends React.Component {
     console.log("params at Moviedetails render:")
     console.log(this.props.navigation.getParam("movie"))
     const movie = this.props.navigation.getParam("movie")
+    console.log(movie.Poster)
     return (
       <View>
-         <Text> {movie.Title} </Text>
-         <Text> ({movie.Year}) {"\n"} </Text>
+         <Image source={{uri: "https://m.media-amazon.com/images/M/MV5BN2EyZjM3NzUtNWUzMi00MTgxLWI0NTctMzY4M2VlOTdjZWRiXkEyXkFqcGdeQXVyNDUzOTQ5MjY@._V1_SX300.jpg"}} />
+         <Text style={styles.textTitle}> {movie.Title} </Text>
+         <Text style={styles.textYear}> ({movie.Year}) {"\n"} </Text>
          <Text> {movie.Rated}, {movie.Runtime} {"\n"} </Text>
          <Text> {movie.Plot} {"\n"} </Text>
          <Text> {movie.Ratings.map(rating => rating.Source + "\n" + rating.Value + "\n \n")} </Text>
@@ -30,4 +32,18 @@ export default class MovieDetailsScreen extends React.Component {
     );
   }
 }
+
+
+const styles = StyleSheet.create({
+  textTitle: {
+    color: 'black',
+    fontSize: 25, 
+    fontWeight: 'bold'
+  },
+  textYear: {
+    textAlign:'right'
+  }
+});
+
+
 
